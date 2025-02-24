33d185ffa0b61428f5c6f39aa8f41821d2385913
Juan Jesús Silvestre Chávez <134343358+TYisusK@users.noreply.github.com>
2025-02-24 00:28:05 -0600

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  // Inyectamos el Router para navegar entre páginas
+  constructor(private router: Router) {}
+
+  // Método para manejar la navegación
+  goTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
